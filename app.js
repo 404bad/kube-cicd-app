5801ce8a03e7140e06bead3b7508eb3de9a0034d
Ubuntu <ubuntu@ip-172-31-26-122.ec2.internal>
2026-04-20 08:49:05 +0000

    update to v2

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('<h1>Kube CI/CD App v1</h1><p>Deployed via GitHub Actions</p>');
+  res.send('<h1>Kube CI/CD App v2</h1><p>Deployed via GitHub Actions</p>');
 });
 
 app.listen(3000, () => {
   console.log('App running on port 3000');
-});
+})i;
